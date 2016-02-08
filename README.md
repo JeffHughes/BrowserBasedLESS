4ce9f835e7612840ab3a21220532726c3ca2ac7b
Jeff Hughes <me@jeffhughes.com>
2016-02-07 22:21:04 -0600

    initial docs

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
+This is very much a work in progress - don't judge me, yet!
+
 # BrowserBasedLESS
-Process dynamic CSS using JQuery.  Because, sometimes a &lt;cigar class="cigar cigar-default"> is just a &lt;cigar/>. 
+Process dynamic CSS using JQuery.
+
+I got tired of decorating buttons as buttons, tables as tables, and sections as sections.  I understand the need for it in a large library; because sometimes an &lt;a /> is a button.  But, most of the time, a &lt;cigar class="cigar cigar-default"> is just a &lt;cigar />
+
+CSS doesn't natively allow extending the properties of one class to another.
+
+But, how hard is it for jquery to do this?
+```
+onload
+$(".1").addClass("2");
+```
+
+So why not write code like this:
+
+```
+html
+<button>Normal Button</button>
+```
+
+instead of like this:
+
+```
+html
+<button class="btn btn-primary">Normal Button</button>
+```
+
+using
+
+```
+css
+<style>
+    .button-primary {
+        @bbless btn;
+        @bbless btn-primary;
+        display: block;
+        margin: 5px;
+    }
+
+    button {
+        @bbless button-primary;
+    }
+</style>
+```
+
+and a couple lines of JQuery magic?
+
+###Features
+- Allow CSS classes to inherit other classes
+- Validate CSS usage with meaningful console errors
+
+@@bbless
+
+cursed
---
+++ bbless/src/bbless/wwwroot/js/bless.js
@@ -0,0 +1,100 @@
+﻿//Browser Based LESS
+//Jeff Hughes
+
+var blessExtendedClasses = new Object();
+
+function getDomain(url) {
+    if (url === null) return "";
+
+    url = url.replace(/https?:\/\/(www.)?/i, '');
+    if (url.indexOf('/') === -1) {
+        return url;
+    }
+
+    return url.split('/')[0];
+}
+
+function recursionCheck(extClass) {
+    if (blessExtendedClasses.hasOwnProperty(extClass)) {
+        console.log("recursive found: " + extClass + " " + $("." + extClass).length);
+
+        var appendClasses = blessExtendedClasses[extClass].split(",");
+
+        $.each(appendClasses, function (i, e) {
+            $.each($("." + extClass).get(), function (i2, e2) {
+                var ele = $(e2);
+
+                var attr = $(e2).attr('cursed');
+
+                console.log("attr: " + attr);
+
+                if ($(e2).attr('cursed') === undefined) {
+                    ele.addClass(e.trim());
+                    console.log(extClass + " extends " + blessExtendedClasses[extClass] + " applying to " + i2);
+                } else {
+                    console.log(extClass + " extends " + blessExtendedClasses[extClass] + " cursed on " + i2);
+                }
+            });
+
+            recursionCheck(blessExtendedClasses[extClass]);
+        });
+    }
+}
+
+function ParseClasses(classes) {
+    $.each(classes,
+      function (i, e) {
+          var split1 = e.split("{");
+          var className = split1[0];
+          if (className !== undefined)
+              className = className.trim();
+
+          var AllProps = split1[1];
+          if (AllProps !== undefined)
+              AllProps = AllProps.trim();
+
+          if (AllProps !== undefined) {
+              if (AllProps.indexOf(";") > -1)
+                  var props = AllProps.split(";");
+              else
+                  var props = AllProps;
+
+              $.each(props, function (propIndex, prop) {
+                  if (prop !== undefined)
+                      if (prop.indexOf("@bbless") > -1) {
+                          var extClass = prop.replace("@bbless", "").trim();
+                          console.log(className + " extends " + extClass + " applying to " + $(className).length);
+
+                          $(className).addClass(extClass);
+
+                          recursionCheck(extClass);
+
+                          var key = className.replace(".", "");
+
+                          if (blessExtendedClasses.hasOwnProperty(key)) {
+                              blessExtendedClasses[key] += "," + extClass;
+                          } else {
+                              blessExtendedClasses[key] = extClass;
+                          }
+                      }
+              });
+          }
+      });
+}
+
+$(function () {
+    var classes = $("style").text().split("}");
+    ParseClasses(classes);
+
+    $.each(document.styleSheets, function (i, e) {
+        console.log(i + " " + e.href + " " + getDomain(e.href) + " " + document.location.host);
+
+        if (getDomain(e.href) === document.location.host) {
+            console.log(" found a local style sheet ");
+
+            $.get(e.href, function (data) {
+                ParseClasses(data.split("}"));
+            });
+        }
+    });
+});
